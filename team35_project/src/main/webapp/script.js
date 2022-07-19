268a95fa5815df16029d19667d59f1853c28e8bd
vietnguyen <vietnguyen19@augustana.edu>
2022-07-19 05:39:36 +0000

    Optimize translator tool

--- team35_project/src/main/webapp/script.js
+++ team35_project/src/main/webapp/script.js
@@ -1,60 +1,40 @@
-async function processInput() {
-
-    const resp = await fetch("data.json", {
-        method: 'GET',
-        headers : { 
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-         }});
-    const data = await resp.json();
-    addWord(data);
-
-}
-
-function addWord(data) {
-
+function addWord() {
     const translations = document.getElementById('results');
     if (translations) translations.innerHTML = '';
 
     var input = document.getElementById('inputText').value;
 
-    input = input.trim();
-    input = input.replace("\n", " ");
+    input = input.trim().toLowerCase();
+    input = input.replaceAll("\n", " ");
     input = input.split(" ");
+
     var words = [];
 
     for (var i = 0; i < input.length; i++) {
-        
-        var word = input[i].replace(/[^a-z]/gi, '').toLowerCase();
+        var word = input[i].replace(/[^a-z]/gi, '');
         if (word == "") continue;
         words.push(word);
     }
 
-    var imgSrcs = [];
-
     for (var i = 0; i < words.length; i++) {
         for (var j = 0; j < words[i].length; j++) {
-            imgSrcs.push(data[0][words[i][j]]);
+            translations.appendChild(createListElement("./resources/images/" + words[i][j] + ".png"));
         }
-        imgSrcs.push(data[0][" "]);
+        translations.appendChild(createListElement("empty"));
     }
-
-    for (var i = 0; i < imgSrcs.length; i++) {
-        translations.appendChild(createListElement(imgSrcs[i]));
-    }
-
 }
 async function addTranslation() {
     const responseFromServer = await fetch("/translation", {
-        headers : { 
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-         }}
-        );
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }
+    );
     const results = await responseFromServer.json();
     const translations = document.getElementById('results');
     translations.innerHTML = '';
-  
+
     for (var i = 0; i < results.length; i++) {
         translations.appendChild(
             createListElement(results[i].result));
@@ -74,52 +54,47 @@ function createListElement(source) {
     img.src = source;
     liElement.appendChild(img);
     return liElement;
-  }
+}
 
 /**Quiz Results Function For Special Page */
 function quizResults() {
-	var quizLength = 5;
-	var quizScore = 0;
+    var quizLength = 5;
+    var quizScore = 0;
 
-	if(document.getElementById('correct1').checked)
-	{
-		quizScore++;
-	}
-	if(document.getElementById('correct2').checked)
-	{
-		quizScore++;
-	}
-	if(document.getElementById('correct3').checked)
-	{
-		quizScore++;
-	}
-	if(document.getElementById('correct4').checked)
-	{
-		quizScore++;
-	}
-	if(document.getElementById('correct5').checked)
-	{
-		quizScore++;
-	}
+    if (document.getElementById('correct1').checked) {
+        quizScore++;
+    }
+    if (document.getElementById('correct2').checked) {
+        quizScore++;
+    }
+    if (document.getElementById('correct3').checked) {
+        quizScore++;
+    }
+    if (document.getElementById('correct4').checked) {
+        quizScore++;
+    }
+    if (document.getElementById('correct5').checked) {
+        quizScore++;
+    }
 
-	if(quizScore === 0){
-		alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". A Zero? Ouch...");
-	}
-	else if(quizScore === 1){
-		alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". Not a zero, but should hit the books...");
-	}
-	else if(quizScore === 2){
-		alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". Better than nothing...");
-	}
-	else if(quizScore === 3){
-		alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". 60% isn't too bad.");
-	}
-	else if(quizScore === 4){
-		alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". Not too shabby!");
-	}
-	else if(quizScore === 5){
-		alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". You really know your stuff!");
-	}
-	window.location.reload();
+    if (quizScore === 0) {
+        alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". A Zero? Ouch...");
+    }
+    else if (quizScore === 1) {
+        alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". Not a zero, but should hit the books...");
+    }
+    else if (quizScore === 2) {
+        alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". Better than nothing...");
+    }
+    else if (quizScore === 3) {
+        alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". 60% isn't too bad.");
+    }
+    else if (quizScore === 4) {
+        alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". Not too shabby!");
+    }
+    else if (quizScore === 5) {
+        alert("Your Score For This Quiz Is: " + quizScore + " out of " + quizLength + ". You really know your stuff!");
+    }
+    window.location.reload();
 
 }
